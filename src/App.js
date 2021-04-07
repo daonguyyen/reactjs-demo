bc544aa7cc952985833d883763fcad90c46d7dee
DESKTOP-JS828RS\Admin <mdnguyen928@gmail.com>
2021-04-07 22:39:45 +0700

    state

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import TodoFeature from './features/Todo';
 function App() {
   return (
     <div className="App">
-      {/* <TodoFeature /> */}
+      <TodoFeature />
       <AlbumFeature />
     </div>
   );
--- src/features/Todo/components/TodoList/index.jsx
+++ src/features/Todo/components/TodoList/index.jsx
@@ -1,21 +1,42 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import classnames from 'classnames';
+import './styles.scss';
+
 
 TodoList.propTypes = {
     todoList : PropTypes.array,
+    onTodoClick: PropTypes.func
 };
 
 TodoList.defaultProps = {
     todoList: [],
+    onTodoClick: null
 }
 
-function TodoList({danhsach}) {
+function TodoList({danhsach, onTodoClick}) {
     // const danhsach = props.danhsach
     // const {danhsach} = props
+
+    const handleTodoClick = (todo, idx) => {
+        if(!onTodoClick) return ;
+
+        onTodoClick(todo, idx)
+    }
+
     return (
-        <ul>
-            {danhsach.map(todo => (  //Ung voi 1 todo tao ra 1 jsx (dau ngoac tron)
-                <li key={todo.id}>{todo.title}</li>
+        <ul className="todo-list">
+            {danhsach.map((todo, idx) => (  //Ung voi 1 todo tao ra 1 jsx (dau ngoac tron)
+                <li 
+                    key={todo.id} 
+                    className={classnames({ 
+                        'todo-item' : true,
+                        completed : todo.status === 'completed'
+                    })}
+                    onClick={() => handleTodoClick(todo, idx)}
+                >
+                    {todo.title}
+                </li>
             ))}
         </ul>
     );
